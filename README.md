6e470c301d759d4b5f819237e78f1f8ec794891a
Edoardo Giacomello <edoardo.giacomello@fht.org>
2024-07-11 12:30:09 +0200

    Added noise model generation code from HDN

--- README.md
+++ README.md
@@ -12,59 +12,54 @@ support train: train_shape=(800, 1024, 1024)
 support val: val_shape=(201, 1024, 1024)
 
 ````
+# AI4Life N2V, N2V2 and HDN SUMBISSIONS
+
+### Dataset info
+````
+jump_cell_painting train: train_shape=(413, 4, 540, 540)
+jump_cell_painting val: val_shape=(104, 4, 540, 540)
+w2s train: train_shape=(96, 3, 512, 512)
+w2s val: val_shape=(24, 3, 512, 512)
+hagen train: train_shape=(63, 1024, 1024)
+hagen val: val_shape=(16, 1024, 1024)
+support train: train_shape=(800, 1024, 1024)
+support val: val_shape=(201, 1024, 1024)
+
+````
 
 ## Steps to reproduce
 
-## 1. Download and split data
+## 1. Activate Conda Environment
+
+``` conda env create -f conda.yml ```
+
+``` conda activate n2v ```
+
+``` pip install -r requirements.txt ```
+
+## 2. Download and split data
 
 Dataset urls and save paths are defined in `datasets.yml`.
 
 ``` python datasets.py --split --split_ratio=0.8 --seed=1234567890 ```
 
 
+## 3. Train the N2V/N2V2 models
 
-### N2V EXPERIMENT 1
- Trained N2V on every dataset, considering channels as individual images.
+Use the `train_n2v_careamist.py` script to train each model.
 
- #### Parmeters:
-````bash
-   python train_n2v.py --epochs 180 --output_dir models/run1 --dataset_name jump_cell_painting
-   python train_n2v.py --epochs 180 --output_dir models/run1 --dataset_name w2s
-   python train_n2v.py --epochs 180 --output_dir models/run1 --dataset_name hagen
-   python train_n2v.py --epochs 180 --output_dir models/run1 --dataset_name support
-````
+```
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name hagen
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name jump_cell_painting
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name support
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name w2s
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name hagen --use_n2v2
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name jump_cell_painting  --use_n2v2
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name support --use_n2v2
+python train_n2v_careamist.py --epochs 400 --batch_size=512 --output_dir models/n2v_n2v2 --dataset_name w2s --use_n2v2
 
-#### Results:
-
-| jump_cell_painting | w2s | hagen | support |
-| --- | --- | --- | --- |
-| <img src="models/run1/jump_cell_painting/n2v/lightning_logs/version_1/loss.png" width="400"> | <img src="models/run1/w2s/n2v/lightning_logs/version_1/loss.png" width="400"> | <img src="models/run1/hagen/n2v/lightning_logs/version_1/loss.png" width="400"> | <img src="models/run1/support/n2v/lightning_logs/version_1/loss.png" width="400"> |
-| Likely underfitting | Overfitting | Almost ok (LR too high) | Overfitting | 
-
-#### Hints
-- Hagen is the only dataset that trains correctly, according to the loss...
-- [ ] Try channelwise normalization before training (if using a single model for all channels)
-- [ ] Check the dataset (patch size?)
-- [ ] Try reducing the learning rate
-- [ ] Should we save the best PSNR model (and the last)? or validation loss is enough?
-- [x] Try learning channel-wise
-
-### RUN 2
-
-#### Parameters:
-```bash
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name jump_cell_painting --input_channel 0
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name jump_cell_painting --input_channel 1
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name jump_cell_painting --input_channel 2
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name jump_cell_painting --input_channel 3
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name w2s --input_channel 0
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name w2s --input_channel 1
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name w2s --input_channel 2
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name hagen --input_channel 0
-   python train_n2v.py --epochs 500 --output_dir models/run2 --dataset_name support --input_channel 0
 ```
 
+Model folders will be called `models/n2v_n2v2/[n2v | n2v2]_<dataset_name>_chwise`. 
 
-- Training is now channel-wise, so we have a model for each channel for multichannel datasets (jump_cell and w2s)
-- Learning rate is reduced to 0.5e-3 (halved)
-- Models are running for 500 epochs
+
---
+++ generate_noise_model.py
@@ -0,0 +1,89 @@
+import torch
+from hdn.gaussianMixtureNoiseModel import GaussianMixtureNoiseModel
+from hdn import histNoiseModel
+from hdn.utils import plotProbabilityDistribution
+from datasets import load_datasets_yml
+import tifffile
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import argparse
+import logging as log
+
+log.basicConfig()
+log.getLogger().setLevel(log.INFO)
+
+
+
+def generate_noise_model(
+                            output_root:str, 
+                            gt_path:str, 
+                            gt_name:str, 
+                            dataset_name: str, 
+                            dataset_yml: str,
+                            n_gaussian: int = 3,
+                            n_coeff: int = 2,
+                            bins: int = 250,
+                            signal_bin_index: int = 100,
+                            ):
+
+    device = torch.device("cuda:0")
+
+    dset = [d for d in load_datasets_yml(dataset_yml=dataset_yml) if d['name'] == dataset_name][0]
+
+    out_root = os.path.join(output_root, dataset_name, gt_name)
+    gt_path = gt_path or f'predictions/{dataset_name}/{gt_name}.tiff'
+    
+    log.info(f"Loading signals...")
+    observations = tifffile.imread(dset['path'])
+    datamin, datamax = observations.min(), observations.max()
+    signal = tifffile.imread(gt_path).squeeze()
+    log.info(f"Done.")
+
+    try:
+        log.info(f"Generating histogram...")
+        histogram = histNoiseModel.createHistogram(bins=bins, minVal=datamin, maxVal=datamax, observation=observations, signal=signal)
+        os.makedirs(out_root, exist_ok=True)
+        np.save(os.path.join(out_root, 'histogram.npy'), histogram)
+        histogramFD = histogram[0]
+
+        # Let's look at the histogram-based noise model.
+        plt.xlabel('Observation Bin')
+        plt.ylabel('Signal Bin')
+        plt.imshow(histogramFD**0.25, cmap='gray')
+        plt.show()
+        min_signal=np.percentile(signal, 0.5)
+        max_signal=np.percentile(signal, 99.5)
+        log.info("Minimum Signal Intensity is", min_signal)
+        log.info("Maximum Signal Intensity is", max_signal)
+
+        log.info(f"Training GMM for {dataset_name} with {gt_path} GT:")
+        gaussianMixtureNoiseModel = GaussianMixtureNoiseModel(min_signal = min_signal, max_signal = max_signal, path=out_root, weight = None, n_gaussian = n_gaussian, n_coeff = n_coeff, device = device, min_sigma = 50)
+        gaussianMixtureNoiseModel.train(signal, observations, batchSize = 250000, n_epochs = 2000, learning_rate = 0.1, name = 'GMM', lowerClip = 0.5, upperClip = 99.5)
+        log.info(f"Model saved Successfully")
+        plotProbabilityDistribution(signalBinIndex=signal_bin_index, histogram=histogramFD, gaussianMixtureNoiseModel=gaussianMixtureNoiseModel, min_signal=datamin, max_signal=datamax, n_bin= bins, device=device)
+        plt.savefig(os.path.join(out_root, 'GMM.png'))
+        log.info("Figure saved to " + os.path.join(out_root, 'GMM.png'))
+
+    except Exception as e:
+        log.error(f"ERROR PROCESSING DATASET {dataset_name} and model {gt_name}: {str(e)}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a Noise Model from a noisy dataset and a given ground truth generated from another model, e.g., N2V/N2V2")
+    parser.add_argument('--output_root', type=str, default="noise_models/", help='Output root folder to store noise models. Each Noise model will be saved in <output_root>/<dataset_name>/<gt_name>/[GMM.npz|histogram.npy]')
+    parser.add_argument('--gt_name', type=str, help='Name of the GT used to learn the model. If bootstrapping, this could be the name of the model used to generate the GTs. This is used to build the save path for noise models.')
+    parser.add_argument('--gt_path', type=str, help='Path of the TIFF ground truth (or upstream model predictions, if bootstrapping) to use for generating the noise model. If None, the default is predictions/<dataset_name>/<gt_name>.tiff')
+    parser.add_argument('--dataset_name', type=str, help='Name of the dataset to use as observations (the full dataset will be used).')
+    parser.add_argument('--dataset_yml', type=str, help='Dataset yml descriptor', default="datasets.yml")
+    parser.add_argument('--n_gaussian', type=int, default=3, help='Number of gaussians to use for Gaussian Mixture Model')
+    parser.add_argument('--n_coeff', type=int, default=2, help='No. of polynomial coefficients for parameterizing the mean, standard deviation and weight of Gaussian components.')
+    parser.add_argument('--bins', type=int, default=250, help='Number of bins to use for geneating the histogram.')
+    parser.add_argument('--signal_bin_index', type=int, default=100, help='Bin index of the signal that is shown in the saved PNG image')
+
+    args = parser.parse_args()
+    generate_noise_model(output_root=args.output_root,
+                         gt_path=args.gt_path,
+                         gt_name=args.gt_name,
+                         dataset_name=args.dataset_name,
+                         dataset_yml=args.dataset_yml
+                        )
