65b5d1ce92251930738a4ad2ada69354e5c37fac
Edoardo Giacomello <edoardo.giacomello@fht.org>
2024-07-23 18:25:03 +0200

    [Add] noise model gen script epochs argument

--- generate_noise_model.py
+++ generate_noise_model.py
@@ -25,6 +25,7 @@ def generate_noise_model(
                             n_coeff: int = 2,
                             bins: int = 250,
                             signal_bin_index: int = 100,
+                            epochs: int = 2000
                             ):
 
     device = torch.device("cuda:0")
@@ -59,7 +60,7 @@ def generate_noise_model(
 
         log.info(f"Training GMM for {dataset_name} with {gt_path} GT:")
         gaussianMixtureNoiseModel = GaussianMixtureNoiseModel(min_signal = min_signal, max_signal = max_signal, path=out_root+'/', weight = None, n_gaussian = n_gaussian, n_coeff = n_coeff, device = device, min_sigma = 50)
-        gaussianMixtureNoiseModel.train(signal, observations, batchSize = 250000, n_epochs = 2000, learning_rate = 0.1, name = 'GMM', lowerClip = 0.5, upperClip = 99.5)
+        gaussianMixtureNoiseModel.train(signal, observations, batchSize = 250000, n_epochs = epochs, learning_rate = 0.1, name = 'GMM', lowerClip = 0.5, upperClip = 99.5)
         log.info(f"Model saved Successfully")
         plotProbabilityDistribution(signalBinIndex=signal_bin_index, histogram=histogramFD, gaussianMixtureNoiseModel=gaussianMixtureNoiseModel, min_signal=datamin, max_signal=datamax, n_bin= bins, device=device)
         plt.savefig(os.path.join(out_root, 'GMM.png'))
@@ -79,11 +80,13 @@ if __name__ == "__main__":
     parser.add_argument('--n_coeff', type=int, default=2, help='No. of polynomial coefficients for parameterizing the mean, standard deviation and weight of Gaussian components.')
     parser.add_argument('--bins', type=int, default=250, help='Number of bins to use for geneating the histogram.')
     parser.add_argument('--signal_bin_index', type=int, default=100, help='Bin index of the signal that is shown in the saved PNG image')
+    parser.add_argument('--epochs', type=int, default=2000, help='Number of Epochs to train GMM.')
 
     args = parser.parse_args()
     generate_noise_model(output_root=args.output_root,
                          gt_path=args.gt_path,
                          gt_name=args.gt_name,
                          dataset_name=args.dataset_name,
-                         dataset_yml=args.dataset_yml
+                         dataset_yml=args.dataset_yml,
+                         epochs=args.epochs
                         )
