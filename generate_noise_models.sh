af48bbdff14802ff46f6abaf60a6e24fce9b7ddc
Edoardo Giacomello <edoardo.giacomello@fht.org>
2024-07-26 15:07:28 +0200

    Merge conflict

--- generate_noise_models.sh
+++ generate_noise_models.sh
@@ -2,7 +2,5 @@ python generate_noise_model.py --epochs=50 --gt_name=n2v --dataset_name=jump_cel
 python generate_noise_model.py --epochs=50 --gt_name=n2v2 --dataset_name=jump_cell_painting
 python generate_noise_model.py --epochs=50 --gt_name=n2v --dataset_name=w2s
 python generate_noise_model.py --epochs=50 --gt_name=n2v2 --dataset_name=w2s
-# python generate_noise_model.py --epochs=50 --gt_name=n2v --dataset_name=hagen
-# python generate_noise_model.py --epochs=50 --gt_name=n2v2 --dataset_name=hagen
-# python generate_noise_model.py --epochs=50 --gt_name=n2v --dataset_name=support
-# python generate_noise_model.py --epochs=50 --gt_name=n2v2 --dataset_name=support
+python generate_noise_model.py --epochs=50 --gt_name=n2v --dataset_name=w2s
+python generate_noise_model.py --epochs=50 --gt_name=n2v2 --dataset_name=w2s
