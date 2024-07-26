6a6f418094d37058b36b78751a2cff4b0d0e1666
Edoardo Giacomello <edoardo.giacomello@fht.org>
2024-07-26 14:48:02 +0200

    [Add] channelwise noise model training

--- generate_noise_model.py
+++ generate_noise_model.py
@@ -55,65 +55,78 @@ def generate_noise_model(
                             n_coeff: int = 2,
                             bins: int = 250,
                             signal_bin_index: int = 100,
-                            epochs: int = 2000
+                            epochs: int = 2000,
+                            channel: int = None
                             ):
 
     device = torch.device("cuda:0")
 
     dset = [d for d in load_datasets_yml(dataset_yml=dataset_yml) if d['name'] == dataset_name][0]
 
-    out_root = os.path.join(output_root, dataset_name, gt_name)
     gt_path = gt_path or f'predictions/{dataset_name}/{gt_name}.tiff'
     
     log.info(f"Loading signals...")
     observations = tifffile.imread(dset['path'])
-    datamin, datamax = observations.min(), observations.max()
-    signal = tifffile.imread(gt_path).squeeze()
-    log.info(f"Done.")
-
-    try:
-        log.info(f"Generating histogram...")
-        histogram = histNoiseModel.createHistogram(bins=bins, minVal=datamin, maxVal=datamax, observation=observations, signal=signal)
-        os.makedirs(out_root, exist_ok=True)
-        np.save(os.path.join(out_root, 'histogram.npy'), histogram)
-        histogramFD = histogram[0]
-
-        # Let's look at the histogram-based noise model.
-        plt.xlabel('Observation Bin')
-        plt.ylabel('Signal Bin')
-        plt.imshow(histogramFD**0.25, cmap='gray')
-        plt.show()
-        min_signal=np.percentile(signal, 0.5)
-        max_signal=np.percentile(signal, 99.5)
-        log.info(f"Minimum Signal Intensity is {min_signal}")
-        log.info(f"Maximum Signal Intensity is {max_signal}")
-
-        log.info(f"Training GMM for {dataset_name} with {gt_path} GT:")
-        gaussianMixtureNoiseModel = GaussianMixtureNoiseModel(min_signal = min_signal, max_signal = max_signal, path=out_root+'/', weight = None, n_gaussian = n_gaussian, n_coeff = n_coeff, device = device, min_sigma = 50)
-        gaussianMixtureNoiseModel.train(signal, observations, batchSize = 250000, n_epochs = epochs, learning_rate = 0.1, name = 'GMM', lowerClip = 0.5, upperClip = 99.5)
-        log.info(f"Model saved Successfully")
     
     
-    except Exception as e:
-        log.error(f"ERROR PROCESSING DATASET {dataset_name} and model {gt_name}: {str(e)}")
+    channels = range(signal.shape[1]) if signal.ndim == 4 else [0]
+
+    for channel in channels: 
+        
+        assert (channel is None and signal.ndim == 3) or (channel is not None and signal.ndim == 4), "If image is multichannel then select a channel, otherwise set channel to None."
+        if channel is not None:
+            print(f"Selecting channel {channel}")
+            observations = observations[:, channel, ...]
+            signal = signal[:, channel, ...]
+        
+        out_root = os.path.join('noise_models', dataset_name, gt_name, f'channel_{channel if channel is not None else 0}')
+        datamin, datamax = observations.min(), observations.max()
+        signal = tifffile.imread(gt_path).squeeze()
+        log.info(f"Done.")
+
+        try:
+            log.info(f"Generating histogram...")
+            histogram = histNoiseModel.createHistogram(bins=bins, minVal=datamin, maxVal=datamax, observation=observations, signal=signal)
+            os.makedirs(out_root, exist_ok=True)
+            np.save(os.path.join(out_root, 'histogram.npy'), histogram)
+            histogramFD = histogram[0]
+
+            # Let's look at the histogram-based noise model.
+            plt.xlabel('Observation Bin')
+            plt.ylabel('Signal Bin')
+            plt.imshow(histogramFD**0.25, cmap='gray')
+            plt.show()
+            min_signal=np.percentile(signal, 0.5)
+            max_signal=np.percentile(signal, 99.5)
+            log.info(f"Minimum Signal Intensity is {min_signal}")
+            log.info(f"Maximum Signal Intensity is {max_signal}")
+
+            log.info(f"Training GMM for {dataset_name} with {gt_path} GT:")
+            gaussianMixtureNoiseModel = GaussianMixtureNoiseModel(min_signal = min_signal, max_signal = max_signal, path=out_root+'/', weight = None, n_gaussian = n_gaussian, n_coeff = n_coeff, device = device, min_sigma = 50)
+            gaussianMixtureNoiseModel.train(signal, observations, batchSize = 250000, n_epochs = epochs, learning_rate = 0.1, name = 'GMM', lowerClip = 0.5, upperClip = 99.5)
+            log.info(f"Model saved Successfully")
+        
+        
+        except Exception as e:
+            log.error(f"ERROR PROCESSING DATASET {dataset_name} and model {gt_name}: {str(e)}")
 
 
 
-    # Initialize figure and axes
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+        # Initialize figure and axes
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
-    # Animation function
-    def animate(i):
-        plotProbabilityDistribution(ax1, ax2, signalBinIndex=i, histogram=histogramFD, gaussianMixtureNoiseModel=gaussianMixtureNoiseModel, min_signal=datamin, max_signal=datamax, n_bin=bins, device=device)
-        return ax1, ax2
+        # Animation function
+        def animate(i):
+            plotProbabilityDistribution(ax1, ax2, signalBinIndex=i, histogram=histogramFD, gaussianMixtureNoiseModel=gaussianMixtureNoiseModel, min_signal=datamin, max_signal=datamax, n_bin=bins, device=device)
+            return ax1, ax2
 
-    # Create the animation
-    ani = animation.FuncAnimation(fig, animate, frames=bins, interval=200, blit=False)
+        # Create the animation
+        ani = animation.FuncAnimation(fig, animate, frames=bins, interval=200, blit=False)
 
-    # Save the animation as a GIF
-    ani.save(os.path.join(out_root, 'animation.gif'), writer='pillow')
+        # Save the animation as a GIF
+        ani.save(os.path.join(out_root, 'animation.gif'), writer='pillow')
 
-    print(f"Created Noise Model for {gt_name} {dataset_name} at {out_root}")
+        print(f"Created Noise Model for {gt_name} {dataset_name} at {out_root}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a Noise Model from a noisy dataset and a given ground truth generated from another model, e.g., N2V/N2V2")
@@ -126,7 +139,7 @@ if __name__ == "__main__":
     parser.add_argument('--n_coeff', type=int, default=2, help='No. of polynomial coefficients for parameterizing the mean, standard deviation and weight of Gaussian components.')
     parser.add_argument('--bins', type=int, default=250, help='Number of bins to use for geneating the histogram.')
     parser.add_argument('--signal_bin_index', type=int, default=100, help='Bin index of the signal that is shown in the saved PNG image')
-    parser.add_argument('--epochs', type=int, default=2000, help='Number of Epochs to train GMM.')
+    parser.add_argument('--epochs', type=int, default=2000, help='Number of Epochs to train GMM.')   
 
     args = parser.parse_args()
     generate_noise_model(output_root=args.output_root,
